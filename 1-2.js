03f7df6e4cbbfde036a00189378874e0991618ee
Xiuyi <xiuyiye@gmail.com>
2016-08-26 19:46:36 -0400

    chapter 1

---
+++ 1-2.js
@@ -0,0 +1,34 @@
+/**
+ * Created by xiye on 8/26/16.
+ */
+function Lecture(name, teacher) {
+    this.name = name;
+    this.teacher = teacher;
+}
+
+//JavaScript prototype method is used to add methods/properties into object
+Lecture.prototype.display = function() {
+    return this.teacher + " is teaching " + this.name;
+}
+
+function Schedule(lectures) {
+    this.lectures = lectures;
+}
+
+Schedule.prototype.display = function() {
+    var str = "";
+
+    for(var i = 0; i < this.lectures.length; i++) {
+        str += this.lectures[i].display() + " ";
+    }
+
+    return str;
+}
+
+var mySchedule = new Schedule([
+    new Lecture("Gym", "Mr. Smith"),
+    new Lecture("Math", "Mrs. Jones"),
+    new Lecture("English", "TBD")
+])
+
+alert(mySchedule.display());
