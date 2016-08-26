adcf0f50bb8d78473b1adf010170e5a3495e60dc
Xiuyi <xiuyiye@gmail.com>
2016-08-26 19:38:29 -0400

    delete idea

--- 1-1.js
+++ 1-1.js
@@ -1,3 +1,25 @@
 /**
  * Created by xiye on 8/26/16.
  */
+function Lecture(name, teacher) {
+    this.name = name;
+    this.teacher = teacher;
+}
+
+Lecture.prototype.display = function() {
+    return this.teacher + " is teaching " + this.name;
+}
+
+function Schedule(lectures) {
+    this.lectures = lectures;
+}
+
+Schedule.prototype.display = function() {
+    var str = "";
+
+    for(var i = 0; i < this.lectures.length; i++) {
+        str += lectures[i].display() + " ";
+    }
+
+    return str;
+}
