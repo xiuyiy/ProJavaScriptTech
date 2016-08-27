21e49f43559e55906546971d69b1ee83df39ccd7
Xiuyi <xiuyiye@gmail.com>
2016-08-27 10:54:58 -0400

    method overlading

--- 2-5.js
+++ 2-5.js
@@ -7,7 +7,9 @@
 
 //A simple function for sending a message
 function sendMessage(msg, obj) {
+
     //If both a message and an object are provided
+    //JavaScript has this arguments attributes to know the inputs
     if (arguments.length == 2) {
         //Send the message to the object
         obj.handleMsg(msg);
@@ -17,15 +19,15 @@ function sendMessage(msg, obj) {
         //So just display  the default error message
         alert(msg);
     }
-
-    //Call the function with one argument - displaying the message using an alert
-    sendMessage("Hello, World");
-
-    //Otherwise, we can pass in our own object that handles
-    //a different way of displaying informaiton
-    sendMessage("How are you?", {
-        handleMsg: function(msg) {
-            alert("This is a custom messge: " + msg);
-        }
-    })
 }
+
+//Call the function with one argument - displaying the message using an alert
+sendMessage("Hello, World");
+
+//Otherwise, we can pass in our own object that handles
+//a different way of displaying informaiton
+sendMessage("How are you?", {
+    handleMsg: function (msg) {
+        alert("This is a custom messge: " + msg);
+    }
+});
